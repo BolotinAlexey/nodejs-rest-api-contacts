ba87ba176befc56070443af7642b994b2616ab70
Oleksii Bolotin <samkiral73@gmail.com>
2023-06-15 23:38:51 +0200

    add controller addAvatarCtrl

--- controllers/auth/addAvatarCtrl.js
+++ controllers/auth/addAvatarCtrl.js
@@ -1,7 +1,17 @@
 const fs = require("fs/promises")
+const Jimp = require("jimp");
+const path=require("path");
+const { User } = require("../../models");
 
-const addAvatarCtrl = ({file}, res) => {
-    console.log(file);
+const addAvatarCtrl = async ({ file, user }, res) => {
+     console.log(user);
+    const { path: tempPath, filename } = file;
+    const newPath = path.resolve("public/avatars");
+    const avatarURL = path.join(newPath, filename);
+    Jimp.read(tempPath).then(img => img.resize(250, 250).write(avatarURL)).catch(err=>{throw err}).finally(()=> {fs.unlink(tempPath)})
+   
+     await User.findByIdAndUpdate(user.id, { avatarURL });
+    res.json(avatarURL)
 }
 
 module.exports=addAvatarCtrl
