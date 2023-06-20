9aa4291dfca43112820ab80360b53620124ac3bc
Oleksii Bolotin <samkiral73@gmail.com>
2023-06-20 21:40:07 +0200

    add Dockerfile and change bcrypt to bcryptjs for correct doing

--- controllers/auth/signIn.js
+++ controllers/auth/signIn.js
@@ -1,4 +1,4 @@
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { HttpError } = require("../../util");
 const { schemaLogin } = require("../../schemas");
 const jwt = require("jsonwebtoken");
